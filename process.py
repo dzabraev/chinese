b3b426d350c3a7c199df1090d966ca54e1f24597
Maksim Dzabraev <dzabraew@gmail.com>
2019-06-20 21:22:04 +0300

    add pinyin, rus and ch generation in process.py

--- process.py
+++ process.py
@@ -51,6 +51,30 @@ def generate_tex(table):
     tex += '\\end{tabular}'
     return tex
 
+def generate_tex_ch(table):
+    tex = ''
+    tex += '\\begin{tabular}{lll}\n'
+    for c, p, r in table:
+        tex += " \\begin{CJK}{UTF8}{gbsn} %s \\end{CJK}  \\\\\n" % (c)
+    tex += '\\end{tabular}'
+    return tex
+
+def generate_tex_pinyin(table):
+    tex = ''
+    tex += '\\begin{tabular}{lll}\n'
+    for c, p, r in table:
+        tex += " \\begin{CJK}{UTF8}{gbsn} %s \\end{CJK}  \\\\\n" % (p)
+    tex += '\\end{tabular}'
+    return tex
+
+def generate_tex_rus(table):
+    tex = ''
+    tex += '\\begin{tabular}{lll}\n'
+    for c, p, r in table:
+        tex += " \\begin{CJK}{UTF8}{gbsn} %s \\end{CJK}  \\\\\n" % (r)
+    tex += '\\end{tabular}'
+    return tex
+
 def parse_table(fname):
     table = []
     with open(fname) as f:
@@ -67,9 +91,14 @@ def parse_table(fname):
 
 def main():
     output_tex = 'chinese.tex'
-    output_pdf = 'chinese.pdf'
-    tex = ''
-    tex += BEGIN_DOCUMENT
+    output_tex_c = 'chinese-ch.tex'
+    output_tex_p = 'chinese-pinyin.tex'
+    output_tex_r = 'chinese-rus.tex'
+
+    tex = BEGIN_DOCUMENT
+    tex_c = BEGIN_DOCUMENT
+    tex_p = BEGIN_DOCUMENT
+    tex_r = BEGIN_DOCUMENT
     fnames = [x for x in os.listdir('.') if x.endswith('.txt')]
     for fname in fnames:
         lesson_num = fname.replace('.txt', '').replace('lesson', '')
@@ -77,10 +106,36 @@ def main():
         tex += '\\section{Lesson %s}\n' % lesson_num
         tex += generate_tex(table) + "\n\n"
         tex += "\\newpage"
+
+        tex_c += '\\section{Lesson %s}\n' % lesson_num
+        tex_c += generate_tex_ch(table) + "\n\n"
+        tex_c += "\\newpage"
+
+        tex_p += '\\section{Lesson %s}\n' % lesson_num
+        tex_p += generate_tex_pinyin(table) + "\n\n"
+        tex_p += "\\newpage"
+
+        tex_r += '\\section{Lesson %s}\n' % lesson_num
+        tex_r += generate_tex_rus(table) + "\n\n"
+        tex_r += "\\newpage"
     tex += END_DOCUMENT + '\n'
+    tex_c += END_DOCUMENT + '\n'
+    tex_p += END_DOCUMENT + '\n'
+    tex_r += END_DOCUMENT + '\n'
+
     with open(output_tex, 'w') as f:
         f.write(tex)
     os.system('pdflatex %s' % output_tex)
+
+    with open(output_tex_c, 'w') as f:
+        f.write(tex_c)
+    os.system('pdflatex %s' % output_tex_c)
+    with open(output_tex_p, 'w') as f:
+        f.write(tex_p)
+    os.system('pdflatex %s' % output_tex_p)
+    with open(output_tex_r, 'w') as f:
+        f.write(tex_r)
+    os.system('pdflatex %s' % output_tex_r)
 
 
 if __name__ == "__main__":
